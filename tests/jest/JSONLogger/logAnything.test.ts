863d613d7032a5929b80825493f7c4c1d7d995f8
Victor Godinho <dragonsgamers@hotmail.com.br>
2023-01-18 01:28:57 -0300

    test(Test): :white_check_mark: fix symbol test

--- tests/jest/JSONLogger/logAnything.test.ts
+++ tests/jest/JSONLogger/logAnything.test.ts
@@ -5,6 +5,8 @@ import { JSONLoggerPlugin } from "../../../src";
 describe("Test anything log", () => {
     const logger = new JSONLoggerPlugin("");
     test("Symbol log test", async () => {
-        await expect(logger.logJSON(LogLevel.DEBUG, Symbol("test"))).resolves.toEqual(Symbol("test"));
+        await expect(logger.logJSON(LogLevel.DEBUG, Symbol("test"))).resolves.toEqual(expect.objectContaining({
+            message: "Symbol(test)",
+        }));
     });
 });

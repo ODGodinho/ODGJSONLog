cfa3eac1a87e335b08cc53445c02a1c04cdc8eae
ODGodinho <victor.godinho@plataforma13.com.br>
2025-04-03 14:01:22 -0300

    fix: :bug: fix ODGMessage error allow tls message

--- src/Packages/JSONLoggerPlugin.ts
+++ src/Packages/JSONLoggerPlugin.ts
@@ -8,7 +8,7 @@ import {
     type LoggerPluginInterface,
 } from "@odg/log";
 import {
-    MessageException, type ResponseInterface, type RequestInterface, MessageResponse, ODGMessage,
+    type ResponseInterface, type RequestInterface, MessageResponse, ODGMessage,
 } from "@odg/message";
 import ErrorStackParser from "error-stack-parser";
 
@@ -190,7 +190,7 @@ export class JSONLoggerPlugin implements LoggerPluginInterface {
      * @returns {Promise<ResponseInterface<unknown> | undefined>}
      */
     protected async getResponseMessage(message: unknown): Promise<ResponseInterface<unknown> | undefined> {
-        if (message instanceof MessageException || message instanceof MessageResponse) return message.response;
+        if (ODGMessage.isMessageError(message) || message instanceof MessageResponse) return message.response;
 
         return undefined;
     }
@@ -204,7 +204,7 @@ export class JSONLoggerPlugin implements LoggerPluginInterface {
      * @returns {Promise<RequestInterface<unknown> | undefined>}
      */
     protected async getRequestMessage(message: unknown): Promise<RequestInterface<unknown> | undefined> {
-        if (message instanceof MessageException || message instanceof MessageResponse) return message.request;
+        if (ODGMessage.isMessageError(message) || message instanceof MessageResponse) return message.request;
         if (this.isRequestMessage(message)) return message;
 
         return undefined;
@@ -219,7 +219,7 @@ export class JSONLoggerPlugin implements LoggerPluginInterface {
      * @returns {Promise<boolean>}
      */
     protected async isRequestOrResponseMessage(message: unknown): Promise<boolean> {
-        return message instanceof MessageException
+        return ODGMessage.isMessageError(message)
             || message instanceof MessageResponse
             || this.isRequestMessage(message);
     }
